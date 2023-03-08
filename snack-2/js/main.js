b44f3da747f3829d09ffa144d1bb502685146897
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-03-08 15:10:39 +0100

    Lista degli studenti

---
+++ snack-2/js/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// Array di oggetti (lista degli studenti)
+const listaStudenti = [
+  {
+    id: 213,
+    nome: "Marco della Rovere",
+    voto: 78,
+  },
+  {
+    id: 100,
+    nome: "Paolo Cortellessa",
+    voto: 96,
+  },
+  {
+    id: 250,
+    nome: "Andrea Mantegna",
+    voto: 48,
+  },
+  {
+    id: 145,
+    nome: "Gaia Borromini",
+    voto: 74,
+  },
+  {
+    id: 196,
+    nome: "Luigi Grimaldello",
+    voto: 68,
+  },
+  {
+    id: 102,
+    nome: "Piero della Francesca",
+    voto: 50,
+  },
+  {
+    id: 120,
+    nome: "Francesca da Polentta",
+    voto: 84,
+  },
+];
