1214c83f2aaa358aed04fae194a993648486db43
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-03-08 14:32:03 +0100

    creazione della lista degli invitati

---
+++ snack-1/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const nomiInvitati = [
+  "Brad Pitt",
+  "Johnny Depp",
+  "Lady Gaga",
+  "Cristiano Ronaldo",
+  "Georgina Rodriguez",
+  "Chiara Ferragni",
+  "Fedez",
+  "George Clooney",
+  "Amal Clooney",
+  "Maneskin",
+];
