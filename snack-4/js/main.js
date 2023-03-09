1263fe8a7ec503871990467e9059769c9a7f77d6
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-03-09 15:47:37 +0100

    Creazione della lista delle squadre

---
+++ snack-4/js/main.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// Lista di 10 squadre 
+const listaSquadre = [
+  {
+    nome: "Chelsea",
+    punti: 0,
+    falli: 0,
+  },
+  {
+    nome: "Liverpool",
+    punti: 0,
+    falli: 0,
+  },
+  {
+    nome: "Real Madrid",
+    punti: 0,
+    falli: 0,
+  },
+  {
+    nome: "Bayern Monaco",
+    punti: 0,
+    falli: 0,
+  },
+  {
+    nome: "Barcellona",
+    punti: 0,
+    falli: 0,
+  },
+  {
+    nome: "Galatasary",
+    punti: 0,
+    falli: 0,
+  },
+  {
+    nome: "Atletico Madrid",
+    punti: 0,
+    falli: 0,
+  },
+  {
+    nome: "Fiorentina",
+    punti: 0,
+    falli: 0,
+  },
+  {
+    nome: "Albinoleffe",
+    punti: 0,
+    falli: 0,
+  },
+  {
+    nome: "Manchester City",
+    punti: 0,
+    falli: 0,
+  },
+];
+
+console.log(listaSquadre);
