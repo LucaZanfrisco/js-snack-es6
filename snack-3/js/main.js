b386d7d31abb579bd3cc6bc01403d23cc32b3ef6
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-03-09 15:05:58 +0100

    Creazione della lista di bici

---
+++ snack-3/js/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const listaBici = [
+  {
+    modello: "Montain Bike",
+    peso: 25,
+  },
+  {
+    modello: "Bici da citta",
+    peso: 12,
+  },
+  {
+    modello: "Bici da campagna",
+    peso: 18,
+  },
+  {
+    modello: "Bici da corsa",
+    peso: 10,
+  },
+  {
+    modello: "Bici pieghevole",
+    peso: 20,
+  },
+];
+
