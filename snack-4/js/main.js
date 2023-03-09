28d64bd32ac5931398deb5a8dd28754e2b243141
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-03-09 16:17:13 +0100

    Aggiunta punti e falli, nuova lista nomi falli

--- snack-4/js/main.js
+++ snack-4/js/main.js
@@ -1,6 +1,19 @@
 "use strict";
+// FUNZIONI
 
-// Lista di 10 squadre 
+// Genera due numeri random che vengono inseriti in un oggetto
+function numeriRandom(min, max) {
+  const stat = {
+    punti: 0,
+    falli: 0,
+  };
+  stat.punti = Math.floor(Math.random() * (max - min + 1)) + min;
+  stat.falli = Math.floor(Math.random() * (max - min + 1)) + min;
+  return stat;
+}
+
+// MAIN
+// Lista di 10 squadre
 const listaSquadre = [
   {
     nome: "Chelsea",
@@ -55,3 +68,37 @@ const listaSquadre = [
 ];
 
 console.log(listaSquadre);
+
+const min = 1;
+const max = 100;
+
+// Crea una nuova lista con punti e falli generati randomicamente
+const nuovaLista = listaSquadre.map((element) => {
+  // Chiamata alla funzione che genera i numeri random
+  const stat = numeriRandom(min, max);
+  element.punti = stat.punti;
+  element.falli = stat.falli;
+  return element;
+});
+
+// Stampa della lista con i punti e falli generati
+console.log(nuovaLista);
+
+// Crea una nuova lista da dove prende solo il nome e i falli commessi
+const listaNomiFalli = nuovaLista.map((element) => {
+  const { nome, falli } = element;
+  return {
+    nome,
+    falli,
+  };
+});
+
+// Stampa in console della lista Nomi e falli
+console.log(listaNomiFalli);
+
+// Seconda versione di stampa in console
+listaNomiFalli.forEach((element) => {
+  for (let key in element) {
+    console.log(`${[key]}: ${element[key]}`);
+  }
+});
