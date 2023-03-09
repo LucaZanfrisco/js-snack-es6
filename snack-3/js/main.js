866dc86ccc5f61524c226533c1eb9922e19d869f
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-03-09 15:35:40 +0100

    Controllo peso, selezione bici peso, stampa

--- snack-3/js/main.js
+++ snack-3/js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Lista delle bici tramite un array di oggetti
 const listaBici = [
   {
     modello: "Montain Bike",
@@ -23,3 +24,19 @@ const listaBici = [
   },
 ];
 
+// Metto in una variabile il peso della prima bici
+let [{peso}] = listaBici;
+
+// Ciclo tutti gli elementi e se trovo una bici più leggera mi salvo il peso
+listaBici.forEach((element) => {
+    if(element.peso < peso){
+        peso = element.peso;
+    }
+});
+
+// Prendo la bici più leggere e la salvo in un array
+const biciLeggera = listaBici.filter(element => element.peso === peso);
+// Salvo il nome del modello in una variabile
+const [{modello}] = biciLeggera;
+// Stampo in console il nome del modello e il peso
+console.log(`Modello: ${modello}, Peso: ${peso}kg`);
